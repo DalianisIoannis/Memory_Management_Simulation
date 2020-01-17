54eaa39dab714d56af3120155c73ce3db2afd5f1
DalianisIoannis <yannispd@ymail.com>
2020-01-17 12:58:26 +0200

    Final Form.

--- Makefile
+++ Makefile
@@ -1,3 +1,26 @@
-comp:
-	@echo " Compile main ...";
-	gcc main.c virtual_memory.c LRU.c WS.c -ggdb3 -o  runner
+CC = gcc
+CFLAGS = -g3 -Wall
+LDFLAGS = -lm
+
+BDIR = bin
+ODIR = build
+IDIR = headers
+SDIR = src
+
+EXECUTABLE = runner
+
+_DEPS = functions.h
+DEPS = $(patsubst %,$(IDIR)/%,$(_DEPS))
+
+_OBJ = main.o Statistics.o WS.o LRU.o virtual_memory.o Address.o IPT.o
+OBJ = $(patsubst %,$(ODIR)/%,$(_OBJ))
+
+$(ODIR)/%.o: $(SDIR)/%.c $(DEPS)
+	$(CC) $(CFLAGS) -c $< -o $@ $(LDFLAGS)
+
+$(EXECUTABLE): $(OBJ)
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+
+clean:
+	rm -f $(ODIR)/*.o
+	# rm -f $(BDIR)/$(EXECUTABLE)
--- headers/functions.h
+++ headers/functions.h
@@ -5,12 +5,10 @@
 
 #define FRAMESIZE 4096
 
-
 typedef struct mem_addr{
     long    serial_number;  // how early did it enter. The smallest num is the earliest
     int     processId;
     int     pageNumber;
-    int     isEmpty;
     int     dirty;          // bool
     char*   op;             // W or R
 } Address;
@@ -40,26 +38,49 @@ typedef struct statistics{
     long pageRequests;      // all requests regardless if the page is in IPT
     long pageFaults;        // pageNumber not in IPT
     long pagesLoaded;       // pages loaded back to disk
-    long pagesReplaced;     // had to replace page due to lack of space for newly fetched page
+    long pagesFound;        // pages fetched found in IPT
+    long pagesReplaced;     // had to replace page from IPT due to lack of space for newly fetched page
     long pagesRead;
     long pagesWrite; 
 } Stats;
 
 
-int virtual_memory(const int, const int, const int, const int);
+int virtual_memory(const char*, const int, const int, const int, const int);
 
-void LRU(int, int, IPT*, long*, Address**);
+void LRU(int, int, IPT*, long*, Address**, Stats*);
 
-void WS(int, int, IPT*, long*, Address**, Wrk_Set*, long*); // 1st long IPTserial_num and 2nd WSserial_num
+void WS(int, int, IPT*, long*, Address**, Wrk_Set*, long*, Stats*);
 
 void WSet_Inserts(int, Wrk_Set*, Address**, long*, int);
 
-int is_pageNum_in_WS(IPT* Table, const int table_frame, Wrk_Set* WSet, int* empty_window);
+int is_pageNum_in_WS(IPT*, const int, Wrk_Set*, int*);
 
-void InsertWSet(const int window_place, Wrk_Set* WSet, Address** adr, long* WSserial_num);
+void InsertWSet(const int, Wrk_Set*, Address**, long*);
 
-void RemoveWSet(const int window_place, Wrk_Set* WSet, Address** adr, long* WSserial_num);
+void RemoveWSet(const int, Wrk_Set*, Address**, long*);
 
-void InsertIPT(const int frame_place, IPT* Table, Address** adr, long* IPTserial_num);
+void InsertIPT(const int, IPT* Table, Address**, long*);
 
-void RemoveIPT(const int frame_place, IPT* Table, Address** adr);
+void RemoveIPT(const int, IPT*);
+
+Wrk_Set* initialWSet(const int);
+
+void freeWSet(Wrk_Set*);
+
+IPT* IPTinit(const int);
+
+void freeIPT(IPT*);
+
+void printIndex(IPT*, Wrk_Set*, Wrk_Set*, const char*);
+
+Address** initAdr(const int , const int, const char*);
+
+int is_pageNum_in_IPT(IPT*, int*, Address**);
+
+int min_priorityIPT(IPT*, const int);
+
+void printStatistics(Stats*);
+
+Stats* stats_init();
+
+void freeAdr(Address**);
